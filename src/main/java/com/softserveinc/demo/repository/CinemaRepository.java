62d4deddd254d134a62c34ff6cc520662791ef9e
ilesev <ilese@softserveinc.com>
2020-02-24 16:56:33 +0200

    Removed unused JpaSpecificationExecutor interface from CinemaRepository.

--- src/main/java/com/softserveinc/demo/repository/CinemaRepository.java
+++ src/main/java/com/softserveinc/demo/repository/CinemaRepository.java
@@ -4,12 +4,11 @@ import com.softserveinc.demo.model.Cinema;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CinemaRepository extends JpaRepository<Cinema, Long>, JpaSpecificationExecutor {
+public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     Page<Cinema> findAllByNameOrHallsOrIsOpenOrMovies_Id(String name, Integer halls, Boolean isOpen, Long id, Pageable pageable);
     Page<Cinema> findAllByNameOrHallsOrIsOpen(String name, Integer halls, Boolean isOpen, Pageable pageable);
